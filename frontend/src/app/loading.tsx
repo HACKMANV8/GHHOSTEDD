75f900401430100c8f8e4517b8e76c09bfc5d14f
Saheb Sandhu <sahebsandhu@MacBookAir.mshome.net>
2025-10-31 22:43:59 +0530

    Updated README with ONNX integration and LoRa receiver pipeline

--- frontend/src/app/loading.tsx
+++
@@ -1,7 +0,0 @@
-﻿export default function Loading() {
-  return (
-    <div className="flex items-center justify-center h-56">
-      <div className="text-green-300">Loading</div>
-    </div>
-  )
-}
